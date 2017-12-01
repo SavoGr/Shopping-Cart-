4fe0f9cf56f490d8d9aacac3025cb49de0603260
Savo Grujicic <sakapb@gmail.com>
2017-12-01 23:41:16 +0000

    Dodat table head

--- index.php
+++ index.php
@@ -7,32 +7,65 @@ require 'script.php';
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-<style type="text/css">
-
-</style>
+<script
+  src="https://code.jquery.com/jquery-3.2.1.js"
+  integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+  crossorigin="anonymous"></script>
 	<title>Shopping Cart By Savo</title>
 
 </head>
 <body>
+
+
 <div class="container">
-	<?php 
-if (mysqli_num_rows($result)>0)	 {
-echo '<div class="row bg-danger">';
-	while ($r = mysqli_fetch_array($result)) {
-		
-		
-		
-	}
-	echo '</div></div>';
+<h1 class="display-3 bd-info text-success" ><span class="display-1 text-info">VoSa</span> Shopping Cart</h1>
+<br>
+<br>
+<div class="row ">
 
 
-}
+<?php
+ if(mysqli_num_rows($result)>0){
+	while ($r=mysqli_fetch_array($result)) {
+	
+ ?>
+ 
+	<div class="col-3 ">
+		<form method="post" action="index.php?action=add&id= <?php echo $r['id']; ?>">
+			
+			<img src="<?php echo 'img/'.$r['image']; ?>" class="img-thumbnail img-fluid">
+			<h2 class="text-success"><?php echo  $r['name']; ?></h2>
+			<h2 class="text-danger"><?php echo $r['price'].'$'; ?></h2>
+			<input type="text" name="quantity" class="form-control" value="0">
+			<input type="hidden" name="hidden_name" value="<?php $r['name'] ?>">
+			<input type="hidden" name="hidden_price" value="<?php $r['price'] ?>">
+			<input type="submit" name="add" class="btn btn-danger" value="Add to Cart" >
+		</form>
+	</div>
 
-	 ?>	
-  
-  </div>
+<?php }
+} ?>
+</div>
+<!--     Ucitavanje katrica
+ -->
+<br>
+<br>
+<div class="table-responsive">
+ 	<table class="table">
+ 		<tr>
+ 			<th>Name of item</th>
+ 			<th>Quantity</th>
+ 			<th>Price</th>
+ 			<th>Total</th>
+ 			<th>Action</th>
+ 		</tr>
+ 	
+ 	</table>
+ </div>
+</div>
 
 
+ 
 		
 
 	
--- script.php
+++ script.php
@@ -3,5 +3,38 @@ session_start();
 $conn = mysqli_connect('localhost','root','','artical');
 $q  = "SELECT * FROM `artical` ";
 $result = mysqli_query($conn,$q);
+//for adding items to shopping cart
+
+
+if (isset($_POST['add'])) {
+	if (isset($_SESSION['shopping_cart']))
+	{$item_arr_id = array_column($_SESSION['shopping_cart'], 'id');
+		if (!in_array($_GET['id'],$item_arr_id)) {
+			$num = count($_SESSION['shopping_cart']);
+			$array_of_items = array(
+			'id' => $_GET['id'],
+			'name' => $_POST['hidden_name'],
+			'price' =>$_POST['hidden_price'] ,
+			'quantity' =>$_POST['quantity'] ,
+		 );
+			$_SESSION['shopping_cart'][$num] = $array_of_items;
+		}
+		else{
+			echo '<script type="text/javascript">alert("Item is already Added")</script>';
+			echo '<script type="text/javascript"> window.location = "index.php"</script>';
+		}
+		
+	}
+	else{
+		$array_of_items = array(
+			'id' => $_GET['id'],
+			'name' => $_POST['hidden_name'],
+			'price' =>$_POST['hidden_price'] ,
+			'quantity' =>$_POST['quantity'] ,
+		 );
+		$_SESSION['shopping_cart'][0] = $array_of_items;
+		
+	}
+}
 
 ?>
